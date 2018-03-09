48d6142a5eb6bf4a811785ad1e2b4e6c63387377
Yasin Aydin <yasin.aydin@erply.com>
2018-03-09 12:39:24 +0200

    converted tasks to jquery

--- js/override.js
+++ js/override.js
@@ -3,70 +3,66 @@
 change the text of the button to a different text
 and the color of the button to a different colour `.
 */
-var greenButton = document.querySelector("a.green-btn")
-greenButton.addEventListener("click", function (event) {
+var greenButton = $("a.green-btn").first()
+greenButton.on("click", function (event) {
     event.preventDefault()
 
-    this.innerText = "Different Text"
-    this.style.backgroundColor = "#c0fefe"
-    this.style.color = "#333"
+    $(this).text("Different Text")
+        .css("backgroundColor", "#c0fefe")
+        .css("color", "#333")
 })
 
 /*
 5. `Link one of the div buttons to your public Facebook or Github profile
 (feel free to select any button you live"`
 */
-var blueButton = document.querySelector(".blue-btn")
-blueButton.innerText = "Github"
-blueButton.href = "https://github.com/yasinaydin"
-
+var blueButton = $(".blue-btn").first()
+blueButton
+    .text("Yasin's Github")
+    .prop("href", "https://github.com/yasinaydin")
 
 /*
 1. `When I hover on the navigation bar of your webpage, 
 I want the hover effect to make the box white and the text inside the box Black`
 */
-var navbarButtons = document.querySelectorAll(".nav.navbar-nav a")
-for (var i = 0; i < navbarButtons.length; i++) {
-    var button = navbarButtons[i];
-
-    var oldColor = button.style.color;
-    var oldBg = button.style["background-color"];
-
-    button.addEventListener("mouseover", function () {
-        this.style.color = "black";
-        this.style["background-color"] = "white";
-    })
-    button.addEventListener("mouseout", function () {
-        this.style.color = oldColor;
-        this.style["background-color"] = oldBg;
-    })
-}
+$(".nav.navbar-nav a").hover(
+    function () {
+        $(this).data("color", $(this).css("color"))
+        $(this).data("bgcolor", $(this).css("background-color"))
+        $(this).css("color", "black")
+        $(this).css("background-color", "white")
+    },
+    function () {
+        $(this).css("color", $(this).data("color"))
+        $(this).css("background-color", $(this).data("bgcolor"))
+    }
+)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-var form = document.querySelector('form.contact-form')
+var form = $("form.contact-form").first()
 
-var input_name = document.querySelector('form input[name=name]')
-var input_email = document.querySelector('form input[name=email]')
-var input_subject = document.querySelector('form input[name=subject]')
-var input_message = document.querySelector('form textarea[name=message]')
+var input_name = $('form input[name=name]').first()
+var input_email = $('form input[name=email]').first()
+var input_subject = $('form input[name=subject]').first()
+var input_message = $('form textarea[name=message]').first()
 
 var alertValues = function () {
     var text = "The data you entered:\n" +
-        "name=" + input_name.value + "\n" +
-        "email=" + input_email.value + "\n" +
-        "subject=" + input_subject.value + "\n" +
-        "message=" + input_message.value + "\n"
+        "name=" + input_name.val() + "\n" +
+        "email=" + input_email.val() + "\n" +
+        "subject=" + input_subject.val() + "\n" +
+        "message=" + input_message.val() + "\n"
     alert(text)
 }
 
 var hasMissingFields = function () {
     var missingFields = []
 
-    if (!input_name.value) missingFields.push("Name")
-    if (!input_email.value) missingFields.push("Email")
-    if (!input_subject.value) missingFields.push("Subject")
-    if (!input_message.value) missingFields.push("Message")
+    if (!input_name.val()) missingFields.push("Name")
+    if (!input_email.val()) missingFields.push("Email")
+    if (!input_subject.val()) missingFields.push("Subject")
+    if (!input_message.val()) missingFields.push("Message")
 
     if (missingFields.length) {
         var text = "You have following fields missing:\n" + missingFields.join(", ")
@@ -76,12 +72,15 @@ var hasMissingFields = function () {
     return missingFields.length > 0
 }
 
-form.addEventListener("submit", function (event) {
+form.on("submit", function (event) {
     /*
     3. `If any field is missing/empty when user clicks submit button on contact form,
     pop up and alert box saying the name of the missing field/fields`
     */
-    if (hasMissingFields()) return
+    if (hasMissingFields()) {
+        event.preventDefault()
+        return
+    }
 
     /*
     2. `When I click the send button in the contact form
@@ -89,5 +88,3 @@ form.addEventListener("submit", function (event) {
     */
     alertValues()
 })
-
-
